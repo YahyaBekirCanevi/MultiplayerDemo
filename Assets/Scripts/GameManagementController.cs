a445ceeeb9a959dc53f6258ec57638a2b72887b6
Yahya Bekir Canevi <yahyabekircanevi2@gmail.com>
2022-09-07 13:08:19 +0300

    v0.1

---
+++ Assets/Scripts/GameManagementController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagementController : MonoBehaviour
+{
+    public static GameManagementController Instance { get; private set; }
+    public float lowestPoint = -10;
+    public GameObject menu;
+    public bool isMenuOpen = true;
+    public Color selectedColor;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu(!isMenuOpen);
+        }
+    }
+    public void ToggleMenu(bool open)
+    {
+        isMenuOpen = open;
+        menu.SetActive(isMenuOpen);
+    }
+    public void ChangeColor(Image image)
+    {
+        selectedColor = image.color;
+        PlayerColorNetwork.Instance.GetSelectedColor(selectedColor);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
